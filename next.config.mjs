b68a058b03c5f326a1a7b9617e24c5526b91bd52
Sina Razaghi <sinarazaghi75@gmail.com>
2025-02-26 21:16:37 -0500

    Configured Next.js for GitHub Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    output: "export",
+    basePath: "/my-game", // Match your GitHub repository name
+    assetPrefix: "/my-game/",
+  };
+  
+  export default nextConfig;
+  
